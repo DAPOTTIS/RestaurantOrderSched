491cf06c88d7489c9c7cbf2c8790dbce1643d30d
DAPOTTIS <mohamedsaafan04@hotmail.com>
2025-03-30 01:05:42 +0200

    initial commit

---
+++ build/CMakeFiles/RestaurantOrderScheduler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RestaurantOrderScheduler.dir/link.d"
+  "CMakeFiles/RestaurantOrderScheduler.dir/src/main.cpp.o"
+  "CMakeFiles/RestaurantOrderScheduler.dir/src/main.cpp.o.d"
+  "RestaurantOrderScheduler"
+  "RestaurantOrderScheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RestaurantOrderScheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
