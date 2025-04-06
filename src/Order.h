f2f9b66fd06869d1aa08d957d35b42be74507c11
DAPOTTIS <mohamedsaafan04@hotmail.com>
2025-04-06 21:06:55 +0200

    ay kalam 2 (made Order class members public for convenience)

--- src/Order.h
+++ src/Order.h
@@ -7,18 +7,21 @@ enum Priority {
 };
 
 class Order {
-private:
-    int prepTime;
-    int arrivalTime;
-    Priority priority;
 public:
-    int getPrepTime() const {return prepTime;}
+    int burstTime;
+    int arrivalTime;
+    int pid;
+    int waitingTime = 0;
+
+    Priority priority;
+
+    int getPrepTime() const {return burstTime;}
     int getArrivalTime() const {return arrivalTime;}
-    void setPrepTime(const int prep) {prepTime = prep;}
+    void setPrepTime(const int prep) {burstTime = prep;}
     void setArrivalTime(const int arrival) {arrivalTime = arrival;}
 
-    Order(int prepTime, int arrivalTime, Priority priority) :
-    prepTime(prepTime), arrivalTime(arrivalTime), priority(priority){}
+    Order(int pid, int prepTime, int arrivalTime, Priority priority) :
+    pid(pid), burstTime(prepTime), arrivalTime(arrivalTime), priority(priority){}
 };
 
 
